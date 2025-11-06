fe6fcb1aaec203b8ff948998e5cd49888b6b06fa
jorge emilio batun alcocer <jorge.batun@tecdesoftware.edu.mx>
2025-11-05 23:41:30 -0600

    ejercicio 5,2 hecho

--- ejercicios.js
+++ ejercicios.js
@@ -312,6 +312,7 @@ function crearMatriz(filas, columnas) {
  */
 function sumaMatriz(matriz) {
   let suma = 0;
+  
   for (let i=0; i < matriz.length; i++){
     for (let j = 0; j < matriz[i].length; j++){
       suma += matriz [i][j];
